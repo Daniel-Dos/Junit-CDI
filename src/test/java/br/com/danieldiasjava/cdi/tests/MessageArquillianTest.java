a02b3eff9daa63e7c8b699347081dc81ccfe3731
Daniel Dias <daniel.dias.analistati@gmail.com>
2021-02-11 22:33:44 -0300

    first commit.

---
+++ src/test/java/br/com/danieldiasjava/cdi/tests/MessageArquillianTest.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License
+ * Copyright © 2021 Daniel Dias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.com.danieldiasjava.cdi.tests;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import br.com.danieldiasjava.cdi.resource.MessageResource;
+
+@ExtendWith(ArquillianExtension.class)
+class MessageArquillianTest {
+
+	@Inject
+	private MessageResource message;
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar")
+				         .addPackages(true, "br/com/danieldiasjava")
+				         .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Test
+	void returnMessage() {
+		message.getMessagem("Daniel");
+	}
+}
